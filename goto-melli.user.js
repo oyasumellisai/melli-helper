49f376b2b1253c56d576d688c22977fbcd9dd76f
oyasumellisai <152113480+oyasumellisai@users.noreply.github.com>
2023-11-30 17:10:33 -0500

    actually fixed @match

--- goto-melli.user.js
+++ goto-melli.user.js
@@ -5,7 +5,7 @@
 // @version      2023.11.30.1
 // @description  Refresh media when somebody fucked up. Disable Nico on spam so recording machine doesn't die. what the fuck am I doing
 // @author       (You)
-// @match        https://cytu.be/r/25_days_of_autism
+// @match        https://witcheffect.com/
 // @downloadURL  https://github.com/oyasumellisai/melli-helper/raw/main/goto-melli.user.js
 // @updateURL    https://github.com/oyasumellisai/melli-helper/raw/main/goto-melli.user.js
 // @grant        none
@@ -17,4 +17,4 @@ var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23
 if (millisTill10 < 0) {
      millisTill10 += 86400000; // it's after the time, try tomorrow.
 }
-setTimeout(function(){window.location = "https://witcheffect.com/"}, millisTill10);
+setTimeout(function(){window.location = "https://cytu.be/r/25_days_of_autism"}, millisTill10);
