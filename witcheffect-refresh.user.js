045983f263ca05f11150036491572a8507dcd90e
oyasumellisai <oyasumellisai@gmail.com>
2023-12-05 12:47:32 -0500

    Refresh witcheffect every half hour

--- witcheffect-refresh.user.js
+++ witcheffect-refresh.user.js
@@ -2,7 +2,7 @@
 // @name         witcheffect-refresh
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/oyasumellisai/melli-helper
-// @version      2023.12.04.0
+// @version      2023.12.05.0
 // @description  Refresh on new media
 // @author       (You)
 // @match        https://witcheffect.com/
@@ -12,11 +12,22 @@
 // ==/UserScript==
 // Not tested on BKT / yay
 
+/*
+    Trying to do this 2 ways
+    - refresh after new video is loaded (this didnt work as I hoped)
+    - refresh on every hour / half hour
+*/
 
-console.log("Helper started.");
+
+console.log("Refresher started.");
+
+// Refresh on every half hour / hour
+var now = new Date();
+var millisTillRefresh = 1800000 - (now - new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)) % 1800000;
+setTimeout(function(){ location.reload(); }, millisTillRefresh);
+
+// Refresh on video load
 window.setTimeout(setupVideoTitleObserver,5000);
-
-
 function setupVideoTitleObserver() {
     console.log("Setting up observer...");
     const targetNode = document.getElementById('videoContainer').getElementsByTagName("iframe")[0];
