9279b7ff743e5b054156f6db5227f48bf1cf4d62
oyasumellisai <152113480+oyasumellisai@users.noreply.github.com>
2024-12-01 21:38:52 -0500

    Update goto-melli.user.js
    Changed @match to work on witcheffect.com/a-stream

--- goto-melli.user.js
+++ goto-melli.user.js
@@ -2,10 +2,10 @@
 // @name         goto-melli
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/oyasumellisai/melli-helper
-// @version      2023.11.30.1
+// @version      2024.12.01
 // @description  Refresh media when somebody fucked up. Disable Nico on spam so recording machine doesn't die. what the fuck am I doing
 // @author       (You)
-// @match        https://witcheffect.com/
+// @match        https://witcheffect.com/*
 // @downloadURL  https://github.com/oyasumellisai/melli-helper/raw/main/goto-melli.user.js
 // @updateURL    https://github.com/oyasumellisai/melli-helper/raw/main/goto-melli.user.js
 // @grant        none
