1b5dcc458347845410428c5c0fd72f87ac95801d
oyasumellisai <152113480+oyasumellisai@users.noreply.github.com>
2024-12-10 23:11:54 -0500

    Change swap time to 23:45

--- goto-melli.user.js
+++ goto-melli.user.js
@@ -13,7 +13,7 @@
 // Not tested on BKT / yay
 
 var now = new Date();
-var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 10, 0, 0) - now;
+var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 45, 0, 0) - now;
 if (millisTill10 < 0) {
      millisTill10 += 86400000; // it's after the time, try tomorrow.
 }
