bd635c926288c2cc05bd8c56c9e468c909d4dd1a
oyasumellisai <152113480+oyasumellisai@users.noreply.github.com>
2024-12-11 18:44:40 -0500

    Fixed destination URL

--- goto-witcheffect.user.js
+++ goto-witcheffect.user.js
@@ -2,7 +2,7 @@
 // @name         goto-witcheffect
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/oyasumellisai/melli-helper
-// @version      2023.11.30.0
+// @version      2024.12.11.0
 // @description  Refresh media when somebody fucked up. Disable Nico on spam so recording machine doesn't die. what the fuck am I doing
 // @author       (You)
 // @match        https://cytu.be/r/25_days_of_autism
@@ -17,4 +17,4 @@ var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18
 if (millisTill10 < 0) {
      millisTill10 += 86400000; // it's after the time, try tomorrow.
 }
-setTimeout(function(){window.location = "https://witcheffect.com/"}, millisTill10);
+setTimeout(function(){window.location = "https://witcheffect.com/a-stream/"}, millisTill10);
